958ab624b052807a448916d25753dd82ca3aaafb
Batman <elliot.nelson4@gmail.com>
2023-12-05 12:09:22 +0100

    visa

--- EgenProvTest/KodKalendern/packet/Lucka5.java
+++ EgenProvTest/KodKalendern/packet/Lucka5.java
@@ -1,9 +1,12 @@
 package packet;
 
+import java.util.Scanner;
+
 public class Lucka5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("hej");
 
 	}
 
